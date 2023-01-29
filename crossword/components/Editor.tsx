8e4a605fd91d6c1721de03d1d33b819012cf60a7
Danny Gelman <dannygelman1@gmail.com>
2023-01-28 23:51:37 -0500

    tailwind

---
+++ crossword/components/Editor.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+export const Editor = (): ReactElement => {
+  return (
+    <div className="flex h-full w-full bg-red-400 absolute">Hello World</div>
+  );
+};
