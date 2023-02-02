b5365e5fd3f872145ec640b5f6abd3530798e05b
Danny Gelman <dannygelman1@gmail.com>
2023-02-01 22:36:07 -0500

    adding panning

--- crossword/components/Editor.tsx
+++ crossword/components/Editor.tsx
@@ -22,36 +22,71 @@ export const Editor = (): ReactElement => {
   const editorRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const [scale, setScale] = useState(1);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [startCoordinates, setStartCoordinates] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     setNeighbors(getNeighbors(boxes[selected], boxes));
   }, [selected]);
 
   useEffect(() => {
+    const preventDefault = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
     const handleWheel = (event: WheelEvent) => {
       event.preventDefault();
-      const newScale =
-        event.deltaY < 0
-          ? scale + 0.01
-          : scale - 0.01 > 0
-          ? scale - 0.01
-          : scale;
+      const newScale = event.deltaY < 0 ? scale + 0.01 : scale - 0.01;
 
       setScale(newScale);
+    };
+
+    const handleMouseDown = (event: MouseEvent) => {
+      if (event.button === 2) {
+        event.preventDefault();
+        setIsDragging(true);
+        setStartCoordinates({ x: event.clientX, y: event.clientY });
+      }
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+      console.log(position, event.clientX, event.clientY, startCoordinates);
+      if (isDragging) {
+        setPosition({
+          x: position.x + (event.clientX - startCoordinates.x) * 0.5,
+          y: position.y + (event.clientY - startCoordinates.y) * 0.5,
+        });
+        setStartCoordinates({ x: event.clientX, y: event.clientY });
+      }
+    };
+
+    const handleMouseUp = () => {
+      if (isDragging) {
+        setIsDragging(false);
+      }
     };
 
     const editor = editorRef.current;
 
     if (editor) {
       editor.addEventListener("wheel", handleWheel);
+      editor.addEventListener("contextmenu", preventDefault);
+      editor.addEventListener("mousedown", handleMouseDown);
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
     }
 
     return () => {
       if (editor) {
         editor.removeEventListener("wheel", handleWheel);
+        editor.removeEventListener("contextmenu", preventDefault);
+        editor.removeEventListener("mousedown", handleMouseDown);
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
       }
     };
-  }, [scale]);
+  }, [scale, position, isDragging, startCoordinates]);
 
   return (
     <div className="flex items-start justify-center h-full w-full bg-red-400 absolute">
@@ -62,10 +97,11 @@ export const Editor = (): ReactElement => {
         <div
           ref={contentRef}
           className="absolute origin-center top-0 left-0 w-full h-full"
-          style={{ transform: `scale(${scale})` }}
+          style={{
+            transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
+          }}
         >
           {boxes.map((box, i) => {
-            console.log(box);
             return (
               <div
                 key={i}
