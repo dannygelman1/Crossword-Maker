021e89740fa3c3b8fc5529512965afb0b500c736
Danny Gelman <dannygelman1@gmail.com>
2023-03-19 13:38:23 -0400

    api change to create fix box when creating game

--- api/src/games/games.service.ts
+++ api/src/games/games.service.ts
@@ -2,21 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { CreateGameInput } from './dto/create-game.input';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Game } from './entities/game.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Box } from 'src/boxes/entities/box.entity';
 
 @Injectable()
 export class GamesService {
   constructor(
     @InjectRepository(Game)
     private gamesRepository: Repository<Game>,
+    private dataSource: DataSource,
   ) {}
 
   async create(createGameInput: CreateGameInput): Promise<Game> {
-    let game = new Game();
+    return this.dataSource.transaction(async (manager) => {
+      const game = manager.create(Game, {
+        slug: createGameInput.slug,
+      });
+      await manager.save(game);
 
-    game.slug = createGameInput.slug;
-    game = await this.gamesRepository.save(game);
-    return game;
+      const box = manager.create(Box, {
+        game_id: game.id,
+        x: 0,
+        y: 0,
+        isblock: false,
+      });
+      await manager.save(box);
+      return game;
+    });
   }
 
   async findOne(slug: string): Promise<Game | null> {
